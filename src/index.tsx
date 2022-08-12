f1250ce0c7afd75a7aa7460b06c3ac9ccf827aab
Shayne Meyer <shayne.meyer@gmail.com>
2022-08-12 06:08:14 -0700

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import blessed from 'blessed';
+import { render } from 'react-blessed';
+import * as dotenv from 'dotenv';
+import { App } from './App';
+import { MemoryRouter } from 'react-router';
+
+global.console = require('inspector').console;
+
+dotenv.config();
+
+const screen = blessed.screen({
+  autoPadding: true,
+  smartCSR: true,
+  sendFocus: true,
+  title: 'Github Manager',
+  cursor: {
+    color: 'black',
+    shape: 'underline',
+    artificial: true,
+    blink: true,
+  },
+});
+
+screen.key(['q', 'C-c'], () => process.exit(0));
+
+render(
+  <MemoryRouter>
+    <App />
+  </MemoryRouter>,
+  screen
+);
